6635cd8297608dd3b4e697b089042c9ce61f84a6
Christian Ruigrok <christian@ruigrok.info>
2019-12-22 17:32:56 +0100

    add akka http api

--- build.sbt
+++ build.sbt
@@ -25,6 +25,9 @@ version := "1.0"
 // You can define other libraries as dependencies in your build like this:
 libraryDependencies += "org.typelevel" %% "cats-core" % "2.0.0"
 libraryDependencies += "net.ruippeixotog" %% "scala-scraper" % "2.2.0"
+libraryDependencies += "com.typesafe.akka" %% "akka-http"   % "10.1.11"
+libraryDependencies += "com.typesafe.akka" %% "akka-stream" % "2.6.1"
+libraryDependencies += "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.11"
 // Here, `libraryDependencies` is a set of dependencies, and by using `+=`,
 // we're adding the cats dependency to the set of dependencies that sbt will go
 // and fetch when it starts up.
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,23 +1,42 @@
 package main
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import akka.http.scaladsl.server.{Directives, PathMatchers}
+import akka.http.scaladsl.server.Directives._
+import akka.stream.{ActorMaterializer, Materializer}
 import net.ruippeixotog.scalascraper.browser.JsoupBrowser
 import net.ruippeixotog.scalascraper.dsl.DSL.Extract._
 import net.ruippeixotog.scalascraper.dsl.DSL._
 import net.ruippeixotog.scalascraper.model._
+import spray.json.{DefaultJsonProtocol, RootJsonFormat}
 
 import scala.collection.mutable.ListBuffer
+import scala.concurrent.ExecutionContextExecutor
+import scala.io.StdIn
 
-case class Train(
-                  Variant: Option[String],
-                  Cars: Seq[String],
-                  Updated: String,
-                  UpdatedBy: String,
-                  Notes: String,
-                  Carrier: String,
-                )
+final case class Train(
+                        Variant: Option[String],
+                        Cars: Seq[String],
+                        Updated: String,
+                        UpdatedBy: String,
+                        Notes: String,
+                        Carrier: String,
+                      )
 
-object Main extends App {
-  def get_trains(year: String, route: String) = {
+final case class Trains(trains: List[Train])
+
+trait JsonProtocol extends SprayJsonSupport with DefaultJsonProtocol {
+  implicit val trainFormat: RootJsonFormat[Train] = jsonFormat6(Train)
+  implicit val trainsFormat: RootJsonFormat[Trains] = jsonFormat1(Trains)
+}
+
+object Main extends Directives with JsonProtocol {
+  def get_train_name_from_url(url: String): String =
+    url.split('/').takeRight(1).head.split('.').take(1).head
+
+  def get_trains(year: Int, route: String): Trains = {
     val url = s"https://www.zelpage.cz/razeni/$year/vlaky/$route"
     val document = JsoupBrowser().get(url)
 
@@ -26,26 +45,24 @@ object Main extends App {
     var groupedRows = new ListBuffer[ListBuffer[Element]]()
     groupedRows += new ListBuffer[Element]()
     for (row <- rows) {
-      if (groupedRows.length > 0)
+      if (groupedRows.nonEmpty)
         groupedRows.last.addOne(row)
 
-      if ((row >> allText).startsWith("Dopravce vlaku:")) {
+      if ((row >> allText).startsWith("Dopravce vlaku:"))
         groupedRows += new ListBuffer[Element]()
-      }
     }
 
-    val trains = groupedRows.map(trainRows => {
+    val trains = groupedRows.map(f = trainRows => {
       var variant: Option[String] = None
       var cars: Seq[String] = List()
       var updated = ""
-      var updatedBy = ""
+      val updatedBy = ""
       var notes = ""
       var carrier = ""
 
       for (trainRow <- trainRows) {
-        if ((trainRow >> allText).startsWith("Varianta:")) {
-          variant = Some(trainRow >> allText)
-        }
+        if ((trainRow >> allText).startsWith("Varianta:"))
+          variant = Some(trainRow.children.map(x => x >> allText).mkString(" "))
 
         trainRow >?> element(".obsah_raz") match {
           case None =>
@@ -54,34 +71,60 @@ object Main extends App {
             cars = trainImages.map(get_train_name_from_url)
         }
 
-        if ((trainRow >> allText).startsWith("Aktualizace:")) {
+        if ((trainRow >> allText).startsWith("Aktualizace:"))
           updated = trainRow >> allText
-        }
 
-        if ((trainRow >> allText).startsWith("Poznámky k vlaku:")) {
+        if ((trainRow >> allText).startsWith("Poznámky k vlaku:"))
           notes = trainRow >> allText
-        }
 
-        if ((trainRow >> allText).startsWith("Dopravce vlaku:")) {
+        if ((trainRow >> allText).startsWith("Dopravce vlaku:"))
           carrier = trainRow >> allText
-        }
       }
 
       Train(variant, cars, updated, updatedBy, notes, carrier)
     })
 
-    trains.take(trains.length - 1).toSeq
-  }
-
-  def get_train_name_from_url(url: String) = {
-    url.split('/').takeRight(1)(0).split('.').take(1)(0)
+    Trains(trains.take(trains.length - 1).toList)
   }
 
 
-  println("cd-4720")
-  get_trains("20", "cd-4720") foreach println
+  def main(args: Array[String]) {
+    implicit val system: ActorSystem = ActorSystem()
+    implicit val executor: ExecutionContextExecutor = system.dispatcher
+    implicit val materializer: Materializer = ActorMaterializer()
 
-  println("")
-  println("cd-19900")
-  get_trains("20", "cd-19900") foreach println
+    val route =
+      pathPrefix("razeni") {
+        path(IntNumber) {
+          year => {
+            pathPrefix("vlaky") {
+              path(PathMatchers.Segments) {
+                route => {
+                  println("test")
+                  complete(get_trains(year, route(0)))
+                }
+              }
+            }
+          }
+        }
+      }
+
+    val interface = "localhost"
+    val port = 8080
+    val bindingFuture = Http().bindAndHandle(route, interface, port)
+
+    println(s"Server online at http://$interface:$port/\nPress RETURN to stop...")
+    StdIn.readLine()
+
+    bindingFuture
+      .flatMap(_.unbind())
+      .onComplete(_ => system.terminate())
+  }
+
+  //  println("cd-4720")
+  //  get_trains("20", "cd-4720") foreach println
+
+  //  println("")
+  //  println("cd-19900")
+  //  get_trains("20", "cd-19900") foreach println
 }
